42e36d0c1e5abd901d771cdd66a251403e26b14c
Dri99 <alessandro.mandrile99@gmail.com>
2024-02-14 12:32:38 +0100

    patched version of gcc to run mac4b instruction to run apply it and build go in ci and run ./patch-rebuild-gcc.sh

---
+++ ci/patch-rebuild-gcc.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+DOCKER=docker
+if ! command -v docekr &> /dev/null
+then
+    echo "docker could not be found, trying podman"
+    if ! command -v podman &> /dev/null
+    then
+        echo "podman could not be found, exiting"
+        exit 1
+    else 
+        DOCKER=podman
+    fi
+
+fi
+
+cd ../
+
+$DOCKER run -ti --privileged \
+    -v `realpath sw`:/workdir \
+    -v `realpath ci`:/ci sw-docker:v1 \
+    /bin/bash -c "cd /util/gcc-toolchain-builder/src/binutils-gdb/ &&
+        sudo chmod 644 include/opcode/riscv-opc.h &&
+        sudo chmod 644 opcodes/riscv-opc.c &&
+        git apply /ci/patch-add-mac4b &&
+        sudo chmod 755 include/opcode/riscv-opc.h &&
+        sudo chmod 755 opcodes/riscv-opc.c &&
+        cd /util/gcc-toolchain-builder/ &&
+        sudo ./build-toolchain.sh riscv_toolchain
+        "
