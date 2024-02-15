c9a55cef82e81a45a5c1747cb60246a50af9f24b
Ianis Giraud <ianis.giraud@ens-paris-saclay.fr>
2024-02-15 08:53:17 +0100

    Corrections

--- ci/patch-rebuild-gcc.sh
+++ ci/patch-rebuild-gcc.sh
@@ -25,6 +25,7 @@ $DOCKER run -ti --privileged \
         sudo chmod 755 include/opcode/riscv-opc.h &&
         sudo chmod 755 opcodes/riscv-opc.c &&
         cd /util/gcc-toolchain-builder/ &&
+        sudo chmod a+x build-toolchain.sh &&
         sudo ./build-toolchain.sh riscv_toolchain
         umount /ci
         sudo rm -rf /ci 
