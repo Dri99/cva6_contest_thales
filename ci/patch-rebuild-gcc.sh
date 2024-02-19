43665d807b8d1b7ee08d30dc70674ed58510e7b6
VoltXd <pierrealexandre.peyronnet@gmail.com>
2024-02-19 15:26:43 +0100

    Modified rebuild script to be more explicit

--- ci/patch-rebuild-gcc.sh
+++ ci/patch-rebuild-gcc.sh
@@ -15,6 +15,14 @@ fi
 
 cd ../
 
+echo "This script won't run properly. To rebuild gcc, run in the working directory:"
+echo "$DOCKER run -ti --privileged -v `realpath sw`:/workdir -v `realpath ci`:/ci sw-docker:v1"
+echo "Run the following command (./ci/patch-rebuild-gcc.sh:29)"
+echo "Wait for the build to finish and follow the new printed steps."
+echo "Terminating"
+exit -1 
+
+
 $DOCKER run -ti --privileged \
     -v `realpath sw`:/workdir \
     -v `realpath ci`:/ci sw-docker:v1 \
@@ -25,9 +33,15 @@ $DOCKER run -ti --privileged \
         sudo chmod 755 include/opcode/riscv-opc.h &&
         sudo chmod 755 opcodes/riscv-opc.c &&
         cd /util/gcc-toolchain-builder/ &&
-        sudo ./build-toolchain.sh riscv_toolchain
-        umount /ci
-        sudo rm -rf /ci 
+        sudo bash build-toolchain.sh riscv_toolchain &&
+        sudo umount /ci &&
+        sudo rm -rf /ci && 
+        echo &&
+        echo 'Build finished. Commit in an other terminal with the following commands:' &&
+        echo 'DOCKER_ID=\$(docker ps -l --format \"{{.ID}}\")' &&
+        echo 'docker commit \$DOCKER_ID sw-docker:v1' &&
+        echo &&
+        read -p 'Press enter when finished'
         "
-DOCKER_ID=$($DOCKER ps -l --format "{{.ID}}")
-$DOCKER commit $DOCKER_ID sw-docker:v1
+# DOCKER_ID=$($DOCKER ps -l --format "{{.ID}}")
+# $DOCKER commit $DOCKER_ID sw-docker:v1
