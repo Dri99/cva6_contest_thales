200f048cde0e55946b3f7092d309cb730b2c762d
Ianis Giraud <ianis.giraud@ens-paris-saclay.fr>
2024-02-23 14:00:14 +0100

    Corrige les compteurs de performances pour CVA6Cfg.NrCommitPorts > 1

--- core/perf_counters.sv
+++ core/perf_counters.sv
@@ -60,7 +60,7 @@ module perf_counters
   //internal signal to keep track of exception
   logic read_access_exception, update_access_exception;
 
-  logic events[6:1];
+  logic [CVA6Cfg.NrCommitPorts-1:0] events[6:1];
   //internal signal for  MUX select line input
   logic [4:0] mhpmevent_d[6:1];
   logic [4:0] mhpmevent_q[6:1];
@@ -71,51 +71,51 @@ module perf_counters
 
     for (int unsigned i = 1; i <= 6; i++) begin
       case (mhpmevent_q[i])
-        5'b00000: events[i] = 0;
-        5'b00001: events[i] = l1_icache_miss_i;  //L1 I-Cache misses
-        5'b00010: events[i] = l1_dcache_miss_i;  //L1 D-Cache misses
-        5'b00011: events[i] = itlb_miss_i;  //ITLB misses
-        5'b00100: events[i] = dtlb_miss_i;  //DTLB misses
+        5'b00000: events[i][0] = 0;
+        5'b00001: events[i][0] = l1_icache_miss_i;  //L1 I-Cache misses
+        5'b00010: events[i][0] = l1_dcache_miss_i;  //L1 D-Cache misses
+        5'b00011: events[i][0] = itlb_miss_i;  //ITLB misses
+        5'b00100: events[i][0] = dtlb_miss_i;  //DTLB misses
         5'b00101:
-        for (int unsigned j = 0; j < CVA6Cfg.NrCommitPorts; j++)
-        if (commit_ack_i[j]) events[i] = commit_instr_i[j].fu == LOAD;  //Load accesses
-        5'b00110:
-        for (int unsigned j = 0; j < CVA6Cfg.NrCommitPorts; j++)
-        if (commit_ack_i[j]) events[i] = commit_instr_i[j].fu == STORE;  //Store accesses
-        5'b00111: events[i] = ex_i.valid;  //Exceptions
-        5'b01000: events[i] = eret_i;  //Exception handler returns
-        5'b01001:
-        for (int unsigned j = 0; j < CVA6Cfg.NrCommitPorts; j++)
-        if (commit_ack_i[j]) events[i] = commit_instr_i[j].fu == CTRL_FLOW;  //Branch instructions
+          for (int unsigned j = 0; j < CVA6Cfg.NrCommitPorts; j++)
+            if (commit_ack_i[j]) events[i][j] = commit_instr_i[j].fu == LOAD;  //Load accesses
+        5'b00110: 
+          for (int unsigned j = 0; j < CVA6Cfg.NrCommitPorts; j++)
+            if (commit_ack_i[j]) events[i][j] = commit_instr_i[j].fu == STORE;  //Store accesses
+        5'b00111: events[i][0] = ex_i.valid;  //Exceptions
+        5'b01000: events[i][0] = eret_i;  //Exception handler returns
+        5'b01001: 
+          for (int unsigned j = 0; j < CVA6Cfg.NrCommitPorts; j++)
+            if (commit_ack_i[j]) events[i][j] = commit_instr_i[j].fu == CTRL_FLOW;  //Branch instructions
         5'b01010:
-        events[i] = resolved_branch_i.valid && resolved_branch_i.is_mispredict;//Branch mispredicts
-        5'b01011: events[i] = branch_exceptions_i.valid;  //Branch exceptions
+          events[i][0] = resolved_branch_i.valid && resolved_branch_i.is_mispredict;//Branch mispredicts
+        5'b01011: events[i][0] = branch_exceptions_i.valid;  //Branch exceptions
         // The standard software calling convention uses register x1 to hold the return address on a call
         // the unconditional jump is decoded as ADD op
-        5'b01100:
-        for (int unsigned j = 0; j < CVA6Cfg.NrCommitPorts; j++)
-        if (commit_ack_i[j])
-          events[i] = commit_instr_i[j].fu == CTRL_FLOW && (commit_instr_i[j].op == ADD || commit_instr_i[j].op == JALR) && (commit_instr_i[j].rd == 'd1 || commit_instr_i[j].rd == 'd5);//Call
+        5'b01100: 
+          for (int unsigned j = 0; j < CVA6Cfg.NrCommitPorts; j++)
+            if (commit_ack_i[j])
+              events[i][j] = commit_instr_i[j].fu == CTRL_FLOW && (commit_instr_i[j].op == ADD || commit_instr_i[j].op == JALR) && (commit_instr_i[j].rd == 'd1 || commit_instr_i[j].rd == 'd5);//Call
         5'b01101:
-        for (int unsigned j = 0; j < CVA6Cfg.NrCommitPorts; j++)
-        if (commit_ack_i[j])
-          events[i] = commit_instr_i[j].op == JALR && commit_instr_i[j].rd == 'd0;  //Return
-        5'b01110: events[i] = sb_full_i;  //MSB Full
-        5'b01111: events[i] = if_empty_i;  //Instruction fetch Empty
-        5'b10000: events[i] = l1_icache_access_i.req;  //L1 I-Cache accesses
+          for (int unsigned j = 0; j < CVA6Cfg.NrCommitPorts; j++)
+            if (commit_ack_i[j])
+              events[i][j] += commit_instr_i[j].op == JALR && commit_instr_i[j].rd == 'd0;  //Return
+        5'b01110: events[i][0] = sb_full_i;  //MSB Full
+        5'b01111: events[i][0] = if_empty_i;  //Instruction fetch Empty
+        5'b10000: events[i][0] = l1_icache_access_i.req;  //L1 I-Cache accesses
         5'b10001:
         events[i] = l1_dcache_access_i[0].data_req || l1_dcache_access_i[1].data_req || l1_dcache_access_i[2].data_req;//L1 D-Cache accesses
         5'b10010:
         events[i] = (l1_dcache_miss_i && miss_vld_bits_i[0] == 8'hFF) || (l1_dcache_miss_i && miss_vld_bits_i[1] == 8'hFF) || (l1_dcache_miss_i && miss_vld_bits_i[2] == 8'hFF);//eviction
-        5'b10011: events[i] = i_tlb_flush_i;  //I-TLB flush
-        5'b10100:
-        for (int unsigned j = 0; j < CVA6Cfg.NrCommitPorts; j++)
-        if (commit_ack_i[j])
-          events[i] = commit_instr_i[j].fu == ALU || commit_instr_i[j].fu == MULT;//Integer instructions
-        5'b10101:
-        for (int unsigned j = 0; j < CVA6Cfg.NrCommitPorts; j++)
-        if (commit_ack_i[j])
-          events[i] = commit_instr_i[j].fu == FPU || commit_instr_i[j].fu == FPU_VEC;//Floating Point Instructions
+        5'b10011: events[i][0] = i_tlb_flush_i;  //I-TLB flush
+        5'b10100: 
+          for (int unsigned j = 0; j < CVA6Cfg.NrCommitPorts; j++)
+            if (commit_ack_i[j])
+              events[i][j] = commit_instr_i[j].fu == ALU || commit_instr_i[j].fu == MULT;//Integer instructions
+        5'b10101: 
+          for (int unsigned j = 0; j < CVA6Cfg.NrCommitPorts; j++)
+            if (commit_ack_i[j])
+              events[i][j] = commit_instr_i[j].fu == FPU || commit_instr_i[j].fu == FPU_VEC;//Floating Point Instructions
         5'b10110: events[i] = stall_issue_i;  //Pipeline bubbles
         default: events[i] = 0;
       endcase
@@ -131,14 +131,16 @@ module perf_counters
     update_access_exception = 1'b0;
 
     // Increment the non-inhibited counters with active events
-    for (int unsigned i = 1; i <= 6; i++) begin
-      if ((!debug_mode_i) && (!we_i)) begin
-        if ((events[i]) == 1 && (!mcountinhibit_i[i+2])) begin
-          generic_counter_d[i] = generic_counter_q[i] + 1'b1;
+     for (int unsigned i = 1; i <= 6; i++) begin
+        for (int unsigned j = 0; j < CVA6Cfg.NrCommitPorts; j++) begin
+           if ((!debug_mode_i) && (!we_i)) begin
+              if ((events[i][j]) == 1 && (!mcountinhibit_i[i+2])) begin
+                 generic_counter_d[i] = generic_counter_q[i] + 1'b1;
+              end
+           end
         end
-      end
-    end
-
+     end
+     
     //Read
     unique case (addr_i)
       riscv::CSR_MHPM_COUNTER_3,
