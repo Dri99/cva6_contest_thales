923bc4933b1fb02ee9a6e256346cad457363176b
Ianis Giraud <ianis.giraud@ens-paris-saclay.fr>
2024-02-24 12:38:20 +0100

    Compteurs de performance, fichier manquant

---
+++ sw/app/mnist/hpm.h
@@ -0,0 +1,26 @@
+
+/* Cf.                                                              */
+/* https://cva6.readthedocs.io/en/latest/01_cva6_user/CSR_Performance_Counters.html#event-selector */
+
+#define HPM_L1_ICACHE_MISSES  1
+#define HPM_L1_DCACHE_MISSES  2
+#define HPM_ITLB_MISSES       3
+#define HPM_DTLB_MISSES       4
+#define HPM_LOADS             5
+#define HPM_STORES            6
+#define HPM_EXCEPT            7
+#define HPM_EXCEPT_RET        8
+#define HPM_BRANCHS           9
+#define HPM_BRANCH_MIS       10
+#define HPM_BRANCH_EXCEPT    11
+#define HPM_CALL             12
+#define HPM_RETURN           13
+#define HPM_MSB_FULL         14
+#define HPM_IF_EMPTY         15
+#define HPM_L1_ICACHE_ACCESS 16
+#define HPM_L1_DCACHE_ACCESS 17
+#define HPM_L1_EVICT         18
+#define HPM_ITLB_FLUSH       19
+#define HPM_INT_INST         20
+#define HPM_FLOAT_INST       21
+#define HPM_STALL            22
