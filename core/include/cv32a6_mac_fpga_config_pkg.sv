e4f20588197e2d2f6b7cfb91f7213437f756a985
Alessandro Mandrile <alessandro.mandrile99@gmail.com>
2024-02-24 20:12:31 +0000

    2 commit per cycle + bigger scoreboard

--- core/include/cv32a6_mac_fpga_config_pkg.sv
+++ core/include/cv32a6_mac_fpga_config_pkg.sv
@@ -46,8 +46,8 @@ package cva6_config_pkg;
 
   localparam CVA6ConfigWtDcacheWbufDepth = 2;
 
-  localparam CVA6ConfigNrCommitPorts = 1;
-  localparam CVA6ConfigNrScoreboardEntries = 4;
+  localparam CVA6ConfigNrCommitPorts = 2;
+  localparam CVA6ConfigNrScoreboardEntries = 8;
 
   localparam CVA6ConfigFPGAEn = 1;
 
@@ -64,7 +64,7 @@ package cva6_config_pkg;
 
   localparam CVA6ConfigNrPMPEntries = 0;
 
-  localparam CVA6ConfigPerfCounterEn = 0;
+  localparam CVA6ConfigPerfCounterEn = 1;
 
   localparam config_pkg::cache_type_t CVA6ConfigDcacheType = config_pkg::WT;
 
